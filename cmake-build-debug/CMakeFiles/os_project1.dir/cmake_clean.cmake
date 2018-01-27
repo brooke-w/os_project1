9ba88a1d9417047866bfaf42f765372f59b6c9dc
Brooke <brookeweborg@yahoo.com>
2018-01-27 12:55:46 -0500

    project created

---
+++ cmake-build-debug/CMakeFiles/os_project1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_project1.dir/main.cpp.o"
+  "os_project1.pdb"
+  "os_project1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/os_project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
